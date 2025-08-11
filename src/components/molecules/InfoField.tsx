5b67335cb4c041ac6c29a5f5a2238fa1b1605af7
Mojtaba-Gholizadeh <mojtaba.leoooo@gmail.com>
2025-08-11 22:00:28 +0330

    second commit

--- src/components/molecules/InfoField.tsx
+++ src/components/molecules/InfoField.tsx
@@ -1,26 +1,26 @@
 interface InfoFieldProps {
-  label: string;
-  value: string;
-  loading: boolean;
+    label: string
+    value: string
+    loading: boolean
 }
 
-const InfoField = ({ label, value, loading }: InfoFieldProps) => {
-  return (
-    <div className="flex gap-2">
-      <h2 className="font-bold text-lg">{label} :</h2>
-      <p className="text-gray-500 mt-0.5">
-        {loading ? (
-          <div className="flex flex-row gap-2 ml-2 mt-3 items-center">
-            <div className="w-2 h-2 rounded-full bg-[#FFE6C9] animate-bounce [animation-delay:.7s]"></div>
-            <div className="w-2 h-2 rounded-full bg-[#FFE6C9] animate-bounce [animation-delay:.3s]"></div>
-            <div className="w-2 h-2 rounded-full bg-[#FFE6C9] animate-bounce [animation-delay:.7s]"></div>
-          </div>
-        ) : (
-          value
-        )}
-      </p>
-    </div>
-  );
-};
+const InfoField = ({label, value, loading}: InfoFieldProps) => {
+    return (
+        <div className="flex gap-2">
+            <h2 className="font-bold text-lg">{label} :</h2>
+            <p className="text-gray-500 mt-0.5">
+                {loading ? (
+                    <span className="flex flex-row gap-2 ml-2 mt-3 items-center">
+                        <span className="w-2 h-2 rounded-full bg-[#FFE6C9] animate-bounce [animation-delay:.7s]"></span>
+                        <span className="w-2 h-2 rounded-full bg-[#FFE6C9] animate-bounce [animation-delay:.3s]"></span>
+                        <span className="w-2 h-2 rounded-full bg-[#FFE6C9] animate-bounce [animation-delay:.7s]"></span>
+                    </span>
+                ) : (
+                    value
+                )}
+            </p>
+        </div>
+    )
+}
 
-export default InfoField;
+export default InfoField
